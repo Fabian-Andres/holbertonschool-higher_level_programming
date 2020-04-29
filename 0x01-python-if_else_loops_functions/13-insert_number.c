a9988df1be6b9da3209ebfee294c0a5204e2fa2c
Fabian-Andres <f4bian.andres@gmail.com>
2020-04-29 04:46:33 +0000

    Answer 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_node - Insert head
+ * @head: Head to insert
+ * @number: to insert
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *aux_head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || head == NULL)
+		return (NULL);
+
+	aux_head = *head;
+
+	if (number == 0)
+	{
+		new_node->n = number;
+		new_node->next = aux_head;
+
+		*head = new_node;
+		return (new_node);
+	}
+
+	while ((aux_head->next)->n < number)
+	{
+		if (aux_head == NULL)
+			return (NULL);
+		aux_head = aux_head->next;
+	}
+
+	new_node->n = number;
+	new_node->next = aux_head->next;
+
+	aux_head->next = new_node;
+
+	return (new_node);
+}
