39a78c8e1052dde3ef958a79a1b6db5ba807cc0d
Fabian Andres <f4bian.andres@gmail.com>
2020-05-07 19:21:35 +0000

    Update exc 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list and search and replace:
-        new_list = []
-        for i in my_list:
-            if i == search:
-                i = replace
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
             new_list.append(i)
-        return new_list
+    return new_list
