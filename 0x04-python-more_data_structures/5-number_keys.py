96d09cf4638300c191e0853add8ef08fdca71bae
Fabian Andres <f4bian.andres@gmail.com>
2020-05-07 17:32:41 +0000

    Answer 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    if a_dictionary:
+        no_keys = 0
+        for i in range(len(a_dictionary)):
+            no_keys += 1
+
+        return no_keys
