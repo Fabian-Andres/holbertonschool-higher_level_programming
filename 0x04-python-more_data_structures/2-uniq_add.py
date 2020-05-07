cd9e16858997729a06758409618ca8975c450dc8
Fabian Andres <f4bian.andres@gmail.com>
2020-05-07 19:13:46 +0000

    Update exc 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list:
-        my_list = list(dict.fromkeys(my_list))
+        new_list = list(dict.fromkeys(my_list))
         add = 0
-        for i in my_list:
+        for i in new_list:
             add += i
         return add
