e1fdcf1f2e165a89e51824bc31cb1cdad1e23534
Fabian Andres <f4bian.andres@gmail.com>
2020-05-08 04:49:42 +0000

    Update exc

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
+def simple_delete(a_dictionary, key=""):
     for k in list(a_dictionary.keys()):
         del a_dictionary[k]
     return a_dictionary
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
-    for k in a_dictionary):
-        new_dictionary[k] = a_dictionary[k] *2
+    for k in a_dictionary.keys()):
+        new_dictionary[k] = ((a_dictionary[k]) * 2)
     return new_dictionary
