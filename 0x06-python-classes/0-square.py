2c804a269ed59a159d51eefb86c3a64fc6bb70c5
Fabian Andres <f4bian.andres@gmail.com>
2020-05-19 23:52:04 +0000

    Square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3
 class Square:
-    pass
+
+    def __init__(self, height="", width=""):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if value.isdigit():
+            self.__height = value
+        else:
+            pass
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if value.isdigit():
+            self.__width = value
+        else:
+            pass
+
+    def getArea(self):
+        return int(self.__height) * int(self.__width)
