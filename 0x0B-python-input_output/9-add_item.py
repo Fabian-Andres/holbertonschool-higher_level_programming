152dac8c8fb0bcac29a52f176b138c82bb2b4634
Fabian Andres <f4bian.andres@gmail.com>
2020-06-04 04:39:09 +0000

    Update 9 exc

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """[load_save_json_file function]"""
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 
 try:
@@ -11,6 +11,6 @@ except:
     l = []
 
 if len(sys.argv) > 1:
-    for s in sys.argv[1:]:
-        l.append(s)
+    for i in sys.argv[1:]:
+        l.append(i)
 save_to_json_file(l, 'add_item.json')
