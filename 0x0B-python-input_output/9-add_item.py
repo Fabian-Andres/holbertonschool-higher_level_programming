89fe0e75320dcb38ce1d04818acc40f1b0d337b1
Fabian Andres <f4bian.andres@gmail.com>
2020-06-04 04:31:43 +0000

    Update exc 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """[load_save_json_file function]"""
-from sys import argv
+from sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
 try:
-    obj = load_from_json_file("add_item.json")
+    l = load_from_json_file('add_item.json')
 except:
-    obj = []
+    l = []
 
-for i in range(1, len(argv)):
-    obj.append(argv[i])
-
-save_to_json_file(obj, "add_item.json")
+if len(sys.argv) > 1:
+    for s in sys.argv[1:]:
+        l.append(s)
+save_to_json_file(l, 'add_item.json')
