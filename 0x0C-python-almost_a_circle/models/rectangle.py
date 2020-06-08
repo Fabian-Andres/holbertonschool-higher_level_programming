ee68d2fb55fbac9223ce8ee23e19178f0e2f892a
Fabian Andres <f4bian.andres@gmail.com>
2020-06-08 19:40:51 +0000

    Answer 3 integer validator

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,19 +15,41 @@ class Rectangle(Base):
         Arguments:
             width {[int]} -- [initialize width]
             height {[int]} -- [initialize height]
-            x {[int]} -- [initialize x]
-            y {[int]} -- [initialize y]
+            x {[int]} -- [initialize position x]
+            y {[int]} -- [initialize coordinate y]
         """
-        # self.integer_validator("width", width)
         self.__width = width
-        # self.integer_validator("height", height)
+        self.integer_validator("width", width)
         self.__height = height
-        # self.integer_validator("x", x)
-        self.x = x
-        # self.integer_validator("y", y)
+        self.integer_validator("height", height)
+        self.__x = x
+        self.integer_validator("x", x)
         self.__y = y
+        self.integer_validator("y", y)
         if id is not None:
             self.id = id
         else:
             super().__init__()
-        
+
+    @staticmethod
+    def integer_validator(name, value):
+        """[Function for integers validation]
+
+        Args:
+            name ([str]): [description]
+            value ([type]): [Value to validate ]
+
+        Raises:
+            TypeError: [must be an integer]
+            ValueError: [must be >= 0]
+        """
+
+        if type(value) != int:
+            raise TypeError("%s must be an integer" % name)
+
+        if name == "x" or name == "y":
+            if value < 0:
+                raise ValueError("%s must be >= 0" % name)
+        else:
+            if value <= 0:
+                raise ValueError("%s must be > 0" % name)
