a0498f75790545856558e8866e9197119eff87de
Fabian Andres <f4bian.andres@gmail.com>
2020-06-09 23:42:11 +0000

    Üpsate setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -47,6 +47,38 @@ class Rectangle(Base):
     def y(self):
         return self.__y
 
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
     @staticmethod
     def integer_validator(name, value):
         """[Function for integers validation]
