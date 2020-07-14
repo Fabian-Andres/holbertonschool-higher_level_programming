7ed64e6969b5f43deb13f82aa51379f0f07df5a8
Fabian Andres <f4bian.andres@gmail.com>
2020-07-14 02:52:02 +0000

    Answer 18 Temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- temperatures
-SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY 'avg_temp' DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
