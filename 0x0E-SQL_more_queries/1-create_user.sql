5206103020adb7d9ae43154658f12d55593a5190
Fabian Andres <f4bian.andres@gmail.com>
2020-07-14 22:46:54 +0000

    Update 1 create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password_0d_1';
+-- create if exist and grant permisions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password_0d_1';
 GRANT PROXI ON root TO 'user_0d_1';
