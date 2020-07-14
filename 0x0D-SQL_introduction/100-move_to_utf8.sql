7e794a83086e9d12f0df2382cccd3e5edc0fe3ee
Fabian Andres <f4bian.andres@gmail.com>
2020-07-14 02:10:39 +0000

    Answer 100 move to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- move to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
