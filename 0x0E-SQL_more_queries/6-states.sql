3f33775fe361871ec58f763fa83a4704b750f251
Fabian Andres <f4bian.andres@gmail.com>
2020-07-15 02:02:00 +0000

    Answer 6 state table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- states table
+-- script that create the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
