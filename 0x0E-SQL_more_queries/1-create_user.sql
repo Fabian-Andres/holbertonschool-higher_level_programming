24a48c8a2fd38d9ee8c9f402f00a071726921722
Fabian Andres <f4bian.andres@gmail.com>
2020-07-15 00:15:20 +0000

    Update 1 create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create user
 -- create if exist and grant permisions
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'password_0d_1';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
