4c8c1757fc7f9a53d0bfe4b345dd83b476772bb3
Fabian Andres <f4bian.andres@gmail.com>
2020-08-08 23:25:47 +0000

    Fix 0 Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,15 @@
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_database = sys.argv[3]
+
 db = MySQLdb.connect(
     host="localhost", port=3306,
-    user=sys.argv[1], passwd=sys.argv[2],
-    db=sys.argv[3])
+    user=db_user, passwd=db_password,
+    db=db_database)
 
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
