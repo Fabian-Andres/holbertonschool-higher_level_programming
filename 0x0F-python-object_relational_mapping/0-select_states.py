82a9d48a8e8e4045e1fd254cf18575c5ec314aef
Fabian Andres <f4bian.andres@gmail.com>
2020-08-08 23:19:48 +0000

    Fix pep8 0 Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ Get all states """
+
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
+db = MySQLdb.connect(
+    host="localhost", port=3306,
+    user=sys.argv[1], passwd=sys.argv[2],
+    db=sys.argv[3])
 
+cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cur.fetchall()
 
