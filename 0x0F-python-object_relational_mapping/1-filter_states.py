32e1f8354b7ff33cdea8f05753c9c7979c1b0a49
Fabian Andres <f4bian.andres@gmail.com>
2020-08-09 03:36:08 +0000

    Answer 1 filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Filter states """
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=db_user, passwd=db_password,
+        db=db_database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
