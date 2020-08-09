ba9200af4e16a8d651d5bad437f10a3889a8666c
Fabian Andres <f4bian.andres@gmail.com>
2020-08-09 18:39:12 +0000

    Answer 5 All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" All cities by state """
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_database = sys.argv[3]
+    db_search = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=db_user, passwd=db_password,
+        db=db_database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT c.name FROM cities AS c\
+                INNER JOIN states AS s ON c.state_id = s.id\
+                WHERE s.name LIKE BINARY %(db_search)s\
+                ORDER BY c.id ASC", {'db_search': db_search})
+    rows = cur.fetchall()
+
+    count = len(rows)
+    for row in rows:
+        if count != 1:
+            print("%s, " % row, end="")
+        else:
+            print("%s" % row)
+        count -= 1
+
+    cur.close()
+    db.close()
