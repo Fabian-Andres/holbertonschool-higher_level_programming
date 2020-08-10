261b1c5edec1d8b1992de6916c036116eb6c1e6f
Fabian Andres <f4bian.andres@gmail.com>
2020-08-10 00:41:30 +0000

    Answer 10 get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,9 +13,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    first_state = session.query(State)
-    .filter(State.name.like(sys.argv[4]))
-    .first()
+    first_state = session.query(State)\
+    .filter(State.name.like(sys.argv[4])).first()
 
     if first_state:
         print(first_state.id)
