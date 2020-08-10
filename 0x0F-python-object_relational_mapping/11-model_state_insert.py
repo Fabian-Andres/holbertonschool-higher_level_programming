46c67dd5141feb8b2a4d16ae2f088fc7fa2b796c
Fabian Andres <f4bian.andres@gmail.com>
2020-08-10 02:32:07 +0000

    Änswer 11 Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name='Louisiana')
+    new_state = State()
+    new_state.name = 'Louisiana'
     session.add(new_state)
     session.commit()
     print(new_state.id)
