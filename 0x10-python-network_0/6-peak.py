0fca62e4ed151501d11c966d6cfda02c0d42aa11
Fabian Andres <f4bian.andres@gmail.com>
2020-08-19 01:38:46 +0000

    [feature] Find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,19 +8,5 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    len_list = len(list_of_integers)
-
-    if len_list == 1:
-        return list_of_integers[0]
-    if list_of_integers[0] >= list_of_integers[1]:
-        return list_of_integers[0]
-    if list_of_integers[len_list - 1] >= list_of_integers[len_list - 2]:
-        return list_of_integers[len_list - 1]
-
-    for i in range(1, len(list_of_integers) - 1):
-        next_num = list_of_integers[i + 1]
-        prev_num = list_of_integers[i - 1]
-        current = list_of_integers[i]
-
-        if current > next_num and current > prev_num:
-            return current
+    list_of_integers.sort()
+    return list_of_integers[-1]
