f8e84b655b932ad2ac9f9b1aeba5cceb82816de1
Fabian Andres <f4bian.andres@gmail.com>
2020-08-24 21:00:18 +0000

    [fix] fix pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     """ comment """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print(
-            "Body response:\n\t- type: %s\n\t- content: %s\n\t- utf8 content: %s" %
-            (type(html), html, html.decode('utf-8'))
-        )
+    print(
+        "Body response:\n\t- type: %s\n\t- content: %s\n\t- utf8 content: %s"
+        % (type(html), html, html.decode('utf-8'))
+    )
