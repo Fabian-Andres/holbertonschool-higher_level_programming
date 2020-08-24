1a702bf9c4c6b702963c52aa06f62773cc25e7b9
Fabian Andres <f4bian.andres@gmail.com>
2020-08-24 22:43:14 +0000

    [feature] get header with requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-
-    header_id = req.headers['X-Request-Id']
+    header_id = req.headers.get('X-Request-Id')
     print(header_id)
