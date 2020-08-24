fd99c43db8eb428fcf9aa60b58222277b3692cfd
Fabian Andres <f4bian.andres@gmail.com>
2020-08-24 20:51:36 +0000

    [feature] get hbtn status with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
+    """ comment """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:\n\
-            \t- type: %s\n\
-            \t- content: %s\n\
-            \t- utf8: %s" % (type(html), html, html.decode('utf-8')))
+        print(
+            "Body response:\n\t- type: %s\n\t- content: %s\n\t- utf8: %s" %
+            (type(html), html, html.decode('utf-8'))
+        )
