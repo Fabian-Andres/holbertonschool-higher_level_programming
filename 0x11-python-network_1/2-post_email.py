b87cd9d3335591e1e8a21f456b8ded745023ee0e
Fabian Andres <f4bian.andres@gmail.com>
2020-08-24 21:48:16 +0000

    [feature] send post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Post email """
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    """ init variables """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+
+
+    """ Setting of data """
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+
+    """ Seting of request """
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
