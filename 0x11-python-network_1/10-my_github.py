f8cc946b25960372ed2ec979ca91ac846c25b1b1
Fabian Andres <f4bian.andres@gmail.com>
2020-08-25 01:18:01 +0000

    [feature] my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" my github """
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    req = requests.get(url, params={'login': username}, auth=(username, password))
+    data = req.json()
+    print(data.get('id'))
