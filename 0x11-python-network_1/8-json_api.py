334750816eaf151759941118d0d846d06daeb177
Fabian Andres <f4bian.andres@gmail.com>
2020-08-25 00:56:03 +0000

    [feature] json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     """ init variables """
-    url = 'http://ff38d7df.hbtn-cod.io:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
@@ -18,12 +18,10 @@ if __name__ == "__main__":
 
     try:
         data = html.json()
+        if len(data) == 0:
+            print("No result")
+        else:
+            print("[%s] %s" % (data['id'], data['name']))
     except ValueError:
         print("Not a valid JSON")
 
-    if len(data) == 0:
-        print("No result")
-    else:
-        for query_id, query_name in data.items():
-            if query_id != 'id' and query_name != 'name':
-                print("[%s] %s" % (query_id, query_name))
