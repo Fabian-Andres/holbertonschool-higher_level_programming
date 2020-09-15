b776b3cc794be2c5bbaf3bd209dda4b1cf36c3a7
Fabian Andres <f4bian.andres@gmail.com>
2020-09-15 21:26:59 +0000

    [feature] Script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+|   Script that prints prints x times “C is fun”
+*/
+let arg1 = process.argv[2];
+arg1 = parseInt(arg1, 10);
+
+if (Number.isNaN(arg1)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg1; i++) {
+    let str = 'X';
+    for (let j = 0; j < arg1; j++) {
+      str += 'X';
+    }
+    console.log(str);
+  }
+}
