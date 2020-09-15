c6ec981c87d5b6bd51a6d8984ba4539a6dce0417
Fabian Andres <f4bian.andres@gmail.com>
2020-09-15 21:03:58 +0000

    [feature] Script that prints 3 lines using loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+|   Script that prints 3 lines using
+|   an array of string and a loop.
+*/
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const i in myArray) {
+  console.log(myArray[i]);
+}
