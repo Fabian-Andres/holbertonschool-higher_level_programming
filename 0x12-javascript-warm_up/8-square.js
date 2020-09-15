6072a6366506f43257168a60b614f8f3c4e5eb0c
Fabian Andres <f4bian.andres@gmail.com>
2020-09-15 21:29:00 +0000

    [fix] Script that prints a square, update init 'X'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -9,7 +9,7 @@ if (Number.isNaN(arg1)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < arg1; i++) {
-    let str = 'X';
+    let str = '';
     for (let j = 0; j < arg1; j++) {
       str += 'X';
     }
