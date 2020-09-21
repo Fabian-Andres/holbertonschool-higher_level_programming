9287b295f55a27500be015e66745086748aa298d
Fabian Andres <f4bian.andres@gmail.com>
2020-09-21 20:50:05 +0000

    [feature] Create rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Rectangle class */
+class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+module.exports = Rectangle;
