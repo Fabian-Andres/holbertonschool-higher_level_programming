eaa46d19f25416ed0b3b424d951a5126c5b18856
Fabian Andres <f4bian.andres@gmail.com>
2020-09-21 22:11:39 +0000

    [feature] Print rectangle and add double, rotate functions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+/* Rectangle class */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 ||
+        w === undefined || h === undefined) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Print rectangle
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  // Double values
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  // Rotate values
+  rotate () {
+    const w = this.width;
+    const h = this.height;
+    this.width = h;
+    this.height = w;
+  }
+}
+
+module.exports = Rectangle;
