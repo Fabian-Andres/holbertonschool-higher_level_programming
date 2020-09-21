fb01155177a1b725c9418d8df2ffe1ecc8a157f7
Fabian Andres <f4bian.andres@gmail.com>
2020-09-21 22:48:34 +0000

    [fix] semistandar Square class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-/* Rectangle class */
+/* Square class */
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-    constructor(size) { 
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
