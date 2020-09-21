82fba982f58236bc0037f18d68150120577f1b15
Fabian Andres <f4bian.andres@gmail.com>
2020-09-21 21:46:28 +0000

    [feature] Print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* Rectangle class */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 ||
+          w === undefined || h === undefined) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Print rectangle
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = 'X';
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
