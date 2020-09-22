b1b1849503cc74603c45f2103864aa8458ed9e74
Fabian Andres <f4bian.andres@gmail.com>
2020-09-22 00:19:39 +0000

    [feature] Function that converts a number from base 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/** Function that converts a number from base
+ *  10 to another base passed as argument:
+ */
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
