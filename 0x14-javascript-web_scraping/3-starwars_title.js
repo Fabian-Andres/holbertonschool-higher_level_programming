5d582ccf10b04415daddcb848eed9f6be5fe4a0a
Fabian Andres <f4bian.andres@gmail.com>
2020-09-23 04:49:49 +0000

    [feature]  Script that prints the title of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
