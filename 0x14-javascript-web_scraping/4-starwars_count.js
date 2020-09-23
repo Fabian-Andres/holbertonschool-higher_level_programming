a4b1c666af7cfa42d6a7a6bbc5b9d9d8aca030d8
Fabian Andres <f4bian.andres@gmail.com>
2020-09-23 05:19:31 +0000

    [feature] Script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-/**
- * 
- */
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
@@ -10,7 +7,7 @@ request(url, function (error, response, body) {
   } else {
     const res = JSON.parse(body).results;
     let count = 0;
-    for (var key in res) {
+    for (let key in res) {
       if (res[key].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
         count++;
       }
